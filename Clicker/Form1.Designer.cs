94f1eb52f50c8f25759cc1bc33d7b3e9e02a5030
payam <payam.shariat@gmail.com>
2019-06-10 00:02:37 +0430

    before add click

---
+++ Clicker/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtHour = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtSec = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblRemaining = new System.Windows.Forms.Label();
+            this.ddList = new System.Windows.Forms.ComboBox();
+            this.txtInterval = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtMouseX = new System.Windows.Forms.TextBox();
+            this.txtMouseY = new System.Windows.Forms.TextBox();
+            this.btnGrab = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(526, 5);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // txtHour
+            // 
+            this.txtHour.Location = new System.Drawing.Point(76, 36);
+            this.txtHour.Margin = new System.Windows.Forms.Padding(2);
+            this.txtHour.Name = "txtHour";
+            this.txtHour.Size = new System.Drawing.Size(32, 20);
+            this.txtHour.TabIndex = 1;
+            this.txtHour.Text = "11";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 36);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Time to Start";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 36);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Time to Start";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 64);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(12, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "x";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(91, 64);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(12, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "y";
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(128, 36);
+            this.txtMin.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(32, 20);
+            this.txtMin.TabIndex = 2;
+            this.txtMin.Text = "30";
+            // 
+            // txtSec
+            // 
+            this.txtSec.Location = new System.Drawing.Point(180, 36);
+            this.txtSec.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSec.Name = "txtSec";
+            this.txtSec.Size = new System.Drawing.Size(32, 20);
+            this.txtSec.TabIndex = 3;
+            this.txtSec.Text = "11";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(170, 5);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Second";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(418, 78);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(2);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(76, 44);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblRemaining
+            // 
+            this.lblRemaining.AutoSize = true;
+            this.lblRemaining.Location = new System.Drawing.Point(426, 154);
+            this.lblRemaining.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRemaining.Name = "lblRemaining";
+            this.lblRemaining.Size = new System.Drawing.Size(24, 13);
+            this.lblRemaining.TabIndex = 5;
+            this.lblRemaining.Text = "test";
+            // 
+            // ddList
+            // 
+            this.ddList.FormattingEnabled = true;
+            this.ddList.Items.AddRange(new object[] {
+            "AM",
+            "PM"});
+            this.ddList.Location = new System.Drawing.Point(225, 36);
+            this.ddList.Margin = new System.Windows.Forms.Padding(2);
+            this.ddList.Name = "ddList";
+            this.ddList.Size = new System.Drawing.Size(62, 21);
+            this.ddList.TabIndex = 4;
+            this.ddList.Text = "PM";
+            // 
+            // txtInterval
+            // 
+            this.txtInterval.Location = new System.Drawing.Point(343, 37);
+            this.txtInterval.Margin = new System.Windows.Forms.Padding(2);
+            this.txtInterval.Name = "txtInterval";
+            this.txtInterval.Size = new System.Drawing.Size(53, 20);
+            this.txtInterval.TabIndex = 1;
+            this.txtInterval.Text = "100";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(338, 5);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Interval (S)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(225, 98);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 44);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtMouseX
+            // 
+            this.txtMouseX.Location = new System.Drawing.Point(31, 91);
+            this.txtMouseX.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMouseX.Name = "txtMouseX";
+            this.txtMouseX.Size = new System.Drawing.Size(42, 20);
+            this.txtMouseX.TabIndex = 1;
+            this.txtMouseX.Text = "500";
+            // 
+            // txtMouseY
+            // 
+            this.txtMouseY.Location = new System.Drawing.Point(86, 92);
+            this.txtMouseY.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMouseY.Name = "txtMouseY";
+            this.txtMouseY.Size = new System.Drawing.Size(44, 20);
+            this.txtMouseY.TabIndex = 1;
+            this.txtMouseY.Text = "500";
+            // 
+            // btnGrab
+            // 
+            this.btnGrab.Location = new System.Drawing.Point(136, 138);
+            this.btnGrab.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrab.Name = "btnGrab";
+            this.btnGrab.Size = new System.Drawing.Size(76, 44);
+            this.btnGrab.TabIndex = 5;
+            this.btnGrab.Text = "Grab";
+            this.btnGrab.UseVisualStyleBackColor = true;
+            this.btnGrab.Click += new System.EventHandler(this.btnGrab_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(584, 197);
+            this.Controls.Add(this.ddList);
+            this.Controls.Add(this.btnGrab);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblRemaining);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtSec);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.txtInterval);
+            this.Controls.Add(this.txtMouseY);
+            this.Controls.Add(this.txtMouseX);
+            this.Controls.Add(this.txtHour);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtHour;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtSec;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblRemaining;
+        private System.Windows.Forms.ComboBox ddList;
+        private System.Windows.Forms.TextBox txtInterval;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtMouseX;
+        private System.Windows.Forms.TextBox txtMouseY;
+        private System.Windows.Forms.Button btnGrab;
+    }
+}
+
---
+++ Clicker/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clicker
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+                // To update the first time.
+                label1.Text = (DateTime.Now).ToString(@"HH\:mm\:ss");
+                var timer = new Timer { Interval = 1000 };
+                timer.Tick += (o, args) =>
+                {
+                    label1.Text = (DateTime.Now).ToString(@"HH\:mm\:ss");
+                };
+                timer.Start();
+            
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+
+            var timer2 = new Timer { Interval = Convert.ToInt32(txtInterval.Text) };
+
+            if (btnStart.Text=="Start")
+            {
+                timer2.Tick += (o, args) =>
+                { 
+                    
+                    string SetDate=  txtHour.Text+":" + txtMin.Text+":" + txtSec.Text+" "+ ddList.SelectedItem.ToString();
+                    DateTime datetime = DateTime.ParseExact(SetDate, "HH:mm:ss tt", null);
+                    Console.WriteLine();
+                    if ((datetime - DateTime.Now).TotalMilliseconds<0)
+                    {
+                        timer2.Stop();
+                        btnStart.Text = "Start";
+                        MessageBox.Show("DONE");
+                    }
+                    else
+                    {
+                        lblRemaining.Text = (datetime - DateTime.Now).TotalMilliseconds.ToString();
+                        }
+                };
+                timer2.Start();
+                btnStart.Text = "Stop";
+                
+            }
+            else
+            {
+                timer2.Stop();
+                btnStart.Text = "Start";
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Win32.POINT p = new Win32.POINT();
+            p.x = Convert.ToInt16(txtMouseX.Text);
+            p.y = Convert.ToInt16(txtMouseY.Text);
+
+            Win32.ClientToScreen(this.Handle, ref p);
+            Win32.SetCursorPos(p.x, p.y);
+            
+        }
+
+
+        private void btnGrab_Click(object sender, EventArgs e)
+        {
+            btnGrab.Text = "Go to Location";
+
+            int dt = 0;
+            
+            txtMouseX.Text = Cursor.Position.X.ToString();
+            txtMouseY.Text = Cursor.Position.Y.ToString();
+            var timer3 = new Timer { Interval = 1000 };
+            timer3.Tick += (o, args) =>
+            {
+                if ((txtMouseX.Text != Cursor.Position.X.ToString()) || (txtMouseY.Text != Cursor.Position.Y.ToString()))
+                {
+                    txtMouseX.Text = Cursor.Position.X.ToString();
+                    txtMouseY.Text = Cursor.Position.Y.ToString();
+                }
+                else
+                {
+                    dt++;
+                    btnGrab.Enabled = false;
+                    btnGrab.Text = "Grabbing ...";
+                    if (dt > 3)
+                    {
+                        btnGrab.Enabled = true;
+                        btnGrab.Text = "Grab";
+                        timer3.Stop();
+                    }
+                }
+                Console.WriteLine(timer3.ToString());
+
+
+            };
+            timer3.Start();
+        }
+    }
+}
